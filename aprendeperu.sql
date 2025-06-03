f28cf3f28160b1fbc66d387deb0ddca27b12f7ed
Omar Pachas <jesuspachas0403@gmail.com>
2025-06-02 22:33:04 -0500

    Acceso a datos corregido

--- aprendeperu.sql
+++ aprendeperu.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS aprendeperu;
+CREATE DATABASE aprendeperu;
 USE aprendeperu;
 
 CREATE TABLE TIPO_EXAMEN (
@@ -11,15 +11,15 @@ CREATE TABLE AREA (
     area 	VARCHAR(50) NOT NULL
 )ENGINE = INNODB;
 
-CREATE TABLE ESTADOEXAMEN (
+CREATE TABLE ESTADO_EXAMEN (
     idestado 	INT PRIMARY KEY AUTO_INCREMENT,
     estado 		VARCHAR(50) NOT NULL
 )ENGINE = INNODB;
 
 CREATE TABLE ALTERNATIVAS (
-    idalternativa INT PRIMARY KEY AUTO_INCREMENT,
-    alternativa VARCHAR(50) NOT NULL,
-    altcorrecta BOOLEAN
+    idalternativa 	INT PRIMARY KEY AUTO_INCREMENT,
+    alternativa 	VARCHAR(150) NOT NULL,
+    altcorrecta 	BOOLEAN NOT NULL
 )ENGINE = INNODB;
 
 CREATE TABLE ESTUDIANTE (
@@ -33,47 +33,54 @@ CREATE TABLE ESTUDIANTE (
 CREATE TABLE PREGUNTAS (
     idpregunta 		INT PRIMARY KEY AUTO_INCREMENT,
     idalternativa   INT NOT NULL,
-    pregunta 		TEXT,
-    valor 			INT,
+    pregunta 		VARCHAR(150) NOT NULL,
+    valor 			DECIMAL(2,0) NOT NULL,
     FOREIGN KEY (idalternativa) REFERENCES ALTERNATIVAS(idalternativa)
 );
 
-CREATE TABLE RESPUESTA_MARCADA (
-    idrespuestam 	INT PRIMARY KEY AUTO_INCREMENT,
-    estado 		VARCHAR(50) NOT NULL
-)ENGINE = INNODB;
-
-CREATE TABLE EVALUACION (
-    idevaluacion INT PRIMARY KEY AUTO_INCREMENT,
-    fecha_inicio DATETIME,
-    fecha_fin DATETIME,
-    tiempo_desarrollo INT,
-    idtipo INT,
-    idarea INT,
-    idestado INT,
-    idpregunta INT,
-    FOREIGN KEY (idtipo) REFERENCES TIPO(idtipo),
-    FOREIGN KEY (idarea) REFERENCES AREA(idarea),
-    FOREIGN KEY (idestado) REFERENCES ESTADOEXAMEN(idestado),
-    FOREIGN KEY (idpregunta) REFERENCES PREGUNTAS_EVALUACION(idpregunta)
-);
-
 CREATE TABLE PREGUNTAS_EVALUACION (
-    idpregunta INT PRIMARY KEY AUTO_INCREMENT,
+	idprgevaluacion INT PRIMARY KEY AUTO_INCREMENT,
+    idpregunta 		INT NOT NULL,
     FOREIGN KEY (idpregunta) REFERENCES PREGUNTAS(idpregunta)
 );
 
+CREATE TABLE RESULTADO (
+    idresultado 	INT PRIMARY KEY AUTO_INCREMENT,
+    puntaje			INT NOT NULL,
+    idestudiante    INT NOT NULL,
+    FOREIGN KEY (idestudiante) REFERENCES ESTUDIANTE(idestudiante)
+)ENGINE = INNODB;
+
+CREATE TABLE RESPUESTA_MARCADA (
+    idrespuestamar	INT PRIMARY KEY AUTO_INCREMENT,
+    idresultado     INT NOT NULL,
+    FOREIGN KEY (idresultado) REFERENCES RESULTADO(idresultado)
+)ENGINE = INNODB;
+
 CREATE TABLE HISTORIALASIGNACION (
     idasignacion INT PRIMARY KEY AUTO_INCREMENT,
-    fecha_asignacion DATE,
-    idevaluacion INT,
-    idestudiante INT,
+    fecha_asignacion DATE NOT NULL,
+    idevaluacion INT NOT NULL,
+    idestudiante INT NOT NULL,
     FOREIGN KEY (idevaluacion) REFERENCES EVALUACION(idevaluacion),
     FOREIGN KEY (idestudiante) REFERENCES ESTUDIANTE(idestudiante)
-);
+)ENGINE = INNODB;
 
-CREATE TABLE RESPUESTAS_MARCADAS (
-	idrespuestam INT PRIMARY KEY AUTO_INCREMENT,
-    idresultado INT NOT NULL,
-    FOREIGN KEY (idresultado) REFERENCES RESULTADO(idresultado)
-);
+CREATE TABLE EVALUACION (
+    idevaluacion 		INT PRIMARY KEY AUTO_INCREMENT,
+    fecha_inicio 		DATETIME NOT NULL,
+    fecha_fin 			DATETIME NOT NULL,
+    tiempo_desarrollo 	INT NULL,
+    idtipo 				INT NOT NULL,
+    idestudiante 		INT NOT NULL,
+    idarea 				INT NOT NULL,
+    idestado 			INT NOT NULL,
+    idprevaluacion	    INT NOT NULL,
+    FOREIGN KEY (idtipo) REFERENCES TIPO(idtipo),
+    FOREIGN KEY (idestudiante) REFERENCES ESTUDIANTE(idestudiante),
+    FOREIGN KEY (idarea) REFERENCES AREA(idarea),
+    FOREIGN KEY (idestado) REFERENCES ESTADOEXAMEN(idestado),
+    FOREIGN KEY (idprevaluacion) REFERENCES PREGUNTAS_EVALUACION(idprevaluacion)
+)ENGINE = INNODB;
+
+
